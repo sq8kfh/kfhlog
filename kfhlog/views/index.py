dc0284f27151eb972a8655668f1f943da4752f70
SQ8KFH <sq8kfh@gmail.com>
2017-03-07 19:22:13 +0100

    template init

--- kfhlog/views/index.py
+++ kfhlog/views/index.py
@@ -3,17 +3,16 @@ from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
-from ..models import MyModel
 
-
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    try:
+@view_config(route_name='index', renderer='../templates/index.jinja2')
+def index_view(request):
+    """try:
         query = request.dbsession.query(MyModel)
         one = query.filter(MyModel.name == 'one').first()
     except DBAPIError:
         return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'kfhlog'}
+    return {'one': one, 'project': 'kfhlog'}"""
+    return {}
 
 
 db_err_msg = """\
