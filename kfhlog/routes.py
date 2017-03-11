d5441ade6f3d6d0ebe791487cc9358df1ffd9b36
SQ8KFH <sq8kfh@gmail.com>
2017-03-11 23:07:05 +0100

    check page

--- kfhlog/routes.py
+++ kfhlog/routes.py
@@ -1,5 +1,7 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('checkwp', '/check/{qsoprofile:[0-9]+}')
+    config.add_route('check', '/check')
     config.add_route('index', '/')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
---
+++ kfhlog/views/check.py
@@ -0,0 +1,55 @@
+import os
+
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.view import view_config
+
+from ..models import Qso, Band, Mode, Profile, Group
+
+def _check(dbsession, call, profile = None):
+    tmp = None
+    req_type = None
+    addrowlink = False
+    name = ''
+    if profile:
+        req_type = Mode
+        name = dbsession.query(Profile).get(profile).name
+        tmp = dbsession.query(Qso.mode, Qso.band).group_by(Qso.mode, Qso.band).filter_by(call=call, qsoprofile=profile).all()
+    else:
+        req_type = Profile
+        addrowlink = True
+        tmp = dbsession.query(Qso.qsoprofile, Qso.band).group_by(Qso.qsoprofile, Qso.band).filter_by(call=call).all() #filter_by(call='SP3CFM').all()
+    if not tmp:
+        return {'message': 'Call %s is not in the log...' % call}
+    col = {x[1] for x in tmp}
+    col = col.union([5,7,8,9,10,11,12,13])  #always show 80m-10m band
+    col = sorted(col)
+    row = {x[0] for x in tmp}
+    row = sorted(row)
+    res = {}
+    for r,c in tmp:
+        res[(r,c)] = True
+
+    rowh = {id: dbsession.query(req_type).get(id).name for id in row}
+    colh = [dbsession.query(Band).get(id).name for id in col]
+    return {'call':call, 'name': name, 'res': res, 'col': col, 'row': row, 'rowh': rowh, 'colh': colh, 'addrowlink': addrowlink}
+
+@view_config(route_name='checkwp', renderer='check.jinja2')
+def checkwp_view(request):
+    qsoprofile = request.matchdict['qsoprofile']
+    tmp = request.dbsession.query(Profile).get(qsoprofile)
+    if not tmp:
+        raise HTTPNotFound()
+    if 'call' in request.params:
+        call = request.params['call']
+        return _check(request.dbsession, call, qsoprofile)
+    return {}
+
+@view_config(route_name='check', renderer='check.jinja2')
+def check_view(request):
+    if 'call' in request.params:
+        call = request.params['call']
+        if 'profile' in request.params:
+            profile = request.params['profile']
+            return _check(request.dbsession, call, profile)
+        return _check(request.dbsession, call)
+    return {}
